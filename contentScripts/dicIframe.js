de47f0d7915372d8467d720b121080cf9484e9d4
sean.lee <weilonge@gmail.com>
2013-11-01 17:31:28 +0800

    Search phrase in Dictionary Dialog.

--- contentScripts/dicIframe.js
+++ contentScripts/dicIframe.js
@@ -1,6 +1,14 @@
+function showDicDialog(dicDom){
+    $('#dicResult').html(dicDom).scrollTop(0);
+}
+
+function hideDicDialog(){
+}
+
 $(function(){
 	chrome.runtime.sendMessage({}, function(response) {
 		console.log(response);
 		$('#dicResult').html(response.dicDom);
 	});
+    Dical.init(showDicDialog, hideDicDialog);
 });
