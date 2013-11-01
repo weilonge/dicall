27325ca97a5b809fbcdec2c0e16809e1c3522fd8
sean.lee <weilonge@gmail.com>
2013-11-01 17:42:07 +0800

    Add error msg.

--- contentScripts/main.js
+++ contentScripts/main.js
@@ -2,7 +2,7 @@ function showDicDialog(dicDom){
     var dicDomStr = dicDom.clone().wrap('<div/>').parent().html();
     chrome.runtime.sendMessage({dicDom: dicDomStr}, function(response) {
         if("ok" !== response.status){
-            console.log("ERROR: chrome.runtime.sendMessage");
+            console.error("ERROR: chrome.runtime.sendMessage");
         }
     });
     var iframeUrl = chrome.extension.getURL("contentScripts/dicIframe.html");
