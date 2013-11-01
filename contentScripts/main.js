4817842cd4043c741b243edb28de50423bf35de6
sean.lee <weilonge@gmail.com>
2013-11-01 17:12:02 +0800

    Move dictionary feature to an isolated file.

--- contentScripts/main.js
+++ contentScripts/main.js
@@ -1,34 +1,3 @@
-function yahooDic(p){
-    var api = 'http://tw.dictionary.search.yahoo.com/search?p=' + p;
-    $.get(api, function(responseText, textStatus, XMLHttpRequest){
-        var dom = $.parseHTML( responseText );
-        var dicDom = $('#web > ol', dom);
-        if( 0 < dicDom.length){
-            showDicDialog(dicDom);
-        }
-    });
-}
-
-function searchDic(selectedStr, e){
-    yahooDic(selectedStr);
-}
-
-function selectHelper(cb){
-	$(document.body).bind('mouseup', function(e){
-        var selection;
-        if (window.getSelection) {
-	        selection = window.getSelection();
-        } else if (document.selection) {
-            selection = document.selection.createRange();
-        }
-        if( selection.toString() !== '' ){
-            cb(selection.toString(), e);
-        }else{
-            hideDicDialog();
-        }
-	});
-}
-
 function showDicDialog(dicDom){
     var dicDomStr = dicDom.clone().wrap('<div/>').parent().html();
     chrome.runtime.sendMessage({dicDom: dicDomStr}, function(response) {
@@ -43,6 +12,6 @@ function hideDicDialog(){
 }
 
 $(function(){
-    selectHelper(searchDic);
+    Dical.init(showDicDialog, hideDicDialog);
     $('body').append('<div id="dicallWrapper"></div>');
 });
